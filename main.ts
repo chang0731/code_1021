fffdece2d6292f181284dd5c885ea7bd856ca0a6
chang0731 <71751403+chang0731@users.noreply.github.com>
2020-10-21 15:37:36 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    for (let I = 0; I <= 4; I++) {
+        for (let j = 0; j <= 4; j++) {
+            if (I % 2 != j % 2) {
+                led.plot(I, j)
+            }
+        }
+    }
+})
